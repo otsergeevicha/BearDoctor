621bd28a73ddbb5f523825f578039e717f565be1
Alexandr Ivanov <otsergeevicha@gmail.com>
2024-10-28 10:52:53 +0300

    Fix noted

--- Assets/GameReadyAPI.cs
+++ Assets/GameReadyAPI.cs
@@ -3,6 +3,18 @@ using YG;
 
 public class GameReadyAPI : MonoBehaviour
 {
-    private void Start() => 
-        YG2.GameReadyAPI();
+    private bool _firstTouch;
+    
+    private void OnTriggerEnter(Collider _)
+    {
+        if (!_firstTouch)
+        {
+            _firstTouch = true;
+#if UNITY_EDITOR
+            Debug.Log($"Game ready API");
+#endif
+            YG2.GameReadyAPI();
+            
+        }
+    }
 }
